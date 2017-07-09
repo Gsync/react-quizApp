42c4049c4308adb5d19230724263d896a8c7b6e4
Khuram Niaz <knshaikh@gmail.com>
2017-07-08 22:14:05 -0600

    create Book Component

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,21 @@
 
     var Quiz = React.createClass({
         render: function () {
-            return <div><h1>Hello React Quiz App</h1></div>;
+            return <div>
+                {this.props.books.map(function(b) {
+                    return <Book title={b} />
+                })}
+            </div>;
+        }
+    });
+
+    var Book = React.createClass({
+        render: function () {
+            return <div><h4>{this.props.title}</h4></div>;
         }
     });
 
 
 ReactDOM.render(
-    <Quiz />, document.body
+    <Quiz books={['The Lord of the Rinds', 'The Great Gutsby']} />, document.getElementById('app')
 );
