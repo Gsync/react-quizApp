9a2b0ea2f7a542e09cbaa56cc724fab0e9384d04
Khuram Niaz <knshaikh@gmail.com>
2017-07-08 21:46:36 -0600

    basic setup  with express server

---
+++ public/js/app.js
@@ -0,0 +1,11 @@
+
+    var Quiz = React.createClass({
+        render: function () {
+            return <div><h1>Hello React Quiz App</h1></div>;
+        }
+    });
+
+
+ReactDOM.render(
+    <Quiz />, document.body
+);
